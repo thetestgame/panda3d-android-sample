b8e4cb7f8e5b63ff0bb5deb4e10e7e2165075248
Jordan Maxwell <nxtlabs13@gmail.com>
2022-02-22 18:15:18 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -55,8 +55,7 @@ setup(
         'bdist_apps': {
             'signing_certificate': 'cert.pem',
             'signing_private_key': 'private.pem',
-            # optional: Panda will otherwise ask passphrase on command-line
-            #'signing_passphrase': 'panda3d_is_cool',
+            'signing_passphrase': 'panda3d_is_cool',
         },
     },
     # Choosing a classifier in the Games category makes it marked a "Game"
